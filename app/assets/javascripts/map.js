a81cea997bc9fa5e2a75926f2d97781477f818a4
Dāgs Ādams Grīnbergs <dagsagrinbergs@gmail.com>
2017-05-09 13:52:45 +0300

    Add popup to leaflet objects

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -26,7 +26,7 @@ function makeMap() {
     attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   });
 
-  var drawnItems = new L.FeatureGroup();
+  drawnItems = new L.FeatureGroup();
   map.addLayer(drawnItems);
 
   baseMaps = {
@@ -150,8 +150,62 @@ function makeMap() {
       }
     }
   }).addTo(map);
-};
 
+  // Truncate value based on number of decimals
+  var _round = function(num, len) {
+      return Math.round(num*(Math.pow(10, len)))/(Math.pow(10, len));
+  };
+  // Helper method to format LatLng object (x.xxxxxx, y.yyyyyy)
+  var strLatLng = function(latlng) {
+      return "("+_round(latlng.lat, 6)+", "+_round(latlng.lng, 6)+")";
+  };
+  // Generate popup content based on layer type
+  // - Returns HTML string, or null if unknown object
+  var getPopupContent = function(layer) {
+      // Marker - add lat/long
+      if (layer instanceof L.Marker) {
+          return strLatLng(layer.getLatLng());
+      // Polygon - area
+      } else if (layer instanceof L.Polygon) {
+          var latlngs = layer._defaultShape ? layer._defaultShape() : layer.getLatLngs(),
+              area = L.GeometryUtil.geodesicArea(latlngs);
+          return "Area: "+L.GeometryUtil.readableArea(area, true);
+      // Polyline - distance
+      } else if (layer instanceof L.Polyline) {
+          var latlngs = layer._defaultShape ? layer._defaultShape() : layer.getLatLngs(),
+              distance = 0;
+          if (latlngs.length < 2) {
+              return "Distance: N/A";
+          } else {
+              for (var i = 0; i < latlngs.length-1; i++) {
+                  distance += latlngs[i].distanceTo(latlngs[i+1]);
+              }
+              return "Distance: "+_round(distance, 2)+" m";
+          }
+      }
+      return null;
+  };
+  // Object created - bind popup to layer, add to feature group
+  map.on(L.Draw.Event.CREATED, function(event) {
+      var layer = event.layer;
+      var content = getPopupContent(layer);
+      if (content !== null) {
+          layer.bindPopup(content);
+      }
+      drawnItems.addLayer(layer);
+  });
+  // Object(s) edited - update popups
+  map.on(L.Draw.Event.EDITED, function(event) {
+      var layers = event.layers,
+          content = null;
+      layers.eachLayer(function(layer) {
+          content = getPopupContent(layer);
+          if (content !== null) {
+              layer.setPopupContent(content);
+          }
+      });
+  });
+};
 
 window.onload = function() {
   var convertToLayer, handleZipFile;
