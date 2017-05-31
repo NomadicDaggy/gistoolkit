0725827f8cc4e6f1342aa5bd16a23f6db5a82dbe
Dāgs Ādams Grīnbergs <dagsagrinbergs@gmail.com>
2017-05-31 19:07:20 +0300

    User self-deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # Executes methods before specified actions.
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+#  before_action :admin_user,     only: :destroy
 
   # Gathers users for user list.
   def index
@@ -51,9 +51,9 @@ class UsersController < ApplicationController
   # Permanently deletes user.
   def destroy
     @user = User.find(params[:id])
-    if @user == current_user
-      @user.destroy
+    if current_user?(@user)
       log_out
+      @user.destroy
       redirect_to root_url
     else
       @user.destroy
